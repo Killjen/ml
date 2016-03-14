4face8d57ae2a07449255bdc20874451f9da22d4
Beinabih <massajonas@googlemail.com>
2016-03-14 10:22:26 +0100

    Update project.py

--- project.py
+++ project.py
@@ -1,17 +1,76 @@
 #!/usr/bin/python2.7
 from sklearn.cross_validation import train_test_split
 from gensim.models.word2vec import Word2Vec
-from sklearn.preprocessing import scale
-from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import scale, MinMaxScaler
+from sklearn.linear_model import SGDClassifier, RidgeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import roc_curve, auc, accuracy_score
 from sklearn.preprocessing import StandardScaler
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.grid_search import GridSearchCV
+from sklearn.feature_extraction.text import TfidfTransformer
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd 
 import os 
 import re
-import tsne
+from string import punctuation
 import seaborn as sns
+
+
+def getSentiment_Tw(conf):
+	'''
+	loads the csv and splits the twitter data in different sentiments
+	'''
+
+	filename = os.path.join('airline-twitter-sentiment','Tweets.csv')
+	tweets = pd.read_csv(filename)
+
+	tw_text = tweets.text
+	tw_name = tweets.name
+	tw_sent = tweets.airline_sentiment
+	tw_sent_conf = tweets.airline_sentiment_confidence
+	tw_air = tweets.airline
+	tw_rt = tweets.retweet_count
+	tw_coord = tweets.tweet_coord
+	tw_time = tweets.tweet_created
+	tw_loc = tweets.tweet_location
+
+	if conf == False :
+		pos_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "positive"]#+tw_air[tweets.airline_sentiment == "positive"]
+		neu_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "neutral"]#+tw_air[tweets.airline_sentiment == "neutral"] 
+		neg_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "negative"]#+tw_air[tweets.airline_sentiment == "negative"]
+	else:
+		pos_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "positive" and tw_sent_conf[i] == 1.0]
+		neu_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "neutral"  and tw_sent_conf[i] == 1.0]
+		neg_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "negative" and tw_sent_conf[i] == 1.0]
+
+	# alltw = [len(neg_text),len(pos_text),len(neu_text)]
+
+	# filtw = [len(neg_text),len(pos_text),len(neu_text)]
+	# plotlen(alltw, filtw)
+
+
+	return pos_text, neu_text, neg_text
+
+
+def get_train_test(sent1, sent2, sent3="none"):
+	'''
+	cross validation
+	'''
+	global roc
+
+	#use 1 for positive sentiment, -1 for negative, 0 for neutral
+	if sent3 =="none" :
+		y = np.concatenate((np.ones(len(sent1)), np.zeros(len(sent2))))
+		x_train, x_test, y_train, y_test = train_test_split(np.concatenate((sent1, sent2)), y, test_size=0.2, random_state=0)
+		roc = True 
+	else:
+		y = np.concatenate((np.ones(len(sent1)), np.zeros(len(sent2)), np.full(len(sent3),-1, dtype=np.int)))
+		x_train, x_test, y_train, y_test = train_test_split(np.concatenate((sent1, sent2, sent3)), y, test_size=0.2, random_state=0)
+		roc = False #roc curve is for binary outputs
+
+	return x_train, x_test, y_train, y_test
 
 def cleanText(corpus, stopWords):
 	#corpus = [re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',z) for z in corpus]
@@ -23,7 +82,7 @@ def cleanText(corpus, stopWords):
 	corpus = [z.strip('\'"') for z in corpus] 
 	corpus = [z.lower().replace('\n','') for z in corpus]
 
-	corpus = [z.split() for z in corpus]
+	corpus = [[y.strip(punctuation) for y in z.split() if y.strip(punctuation) not in stopWords] for z in corpus]
 
 	# for i,z in enumerate(corpus):
 	# 	corpus[i] = [w for w in z if w not in stopWords ]
@@ -61,6 +120,69 @@ def buildWordVector(text, size):
 
 	return vec
 
+def build_and_scale(x_train, x_test, n_dim):
+	'''
+	build all tweet vectors and scale
+	'''
+
+	train_vecs = np.concatenate([buildWordVector(z, n_dim) for z in x_train])
+	train_vecs = scale(train_vecs)
+
+	test_vecs = np.concatenate([buildWordVector(z, n_dim) for z in x_test])
+	test_vecs = scale(test_vecs)
+
+	return train_vecs, test_vecs
+
+def classify(x_train, y_train, x_test, y_test):
+	'''
+	classification with stochastic gradient descent + logistic regression
+	'''
+
+	n_dim = 400
+
+	#Initialize model and build vocab
+	#min_count = ignore all words with total frequency lower than this.
+	global tw_w2v
+	tw_w2v = Word2Vec(size=n_dim, min_count=50, negative=0, window=5) #size = size of the NN layer
+	tw_w2v.build_vocab(x_train)
+
+	#Train the model 
+	tw_w2v.train(x_train)
+
+	train_vecs, test_vecs = build_and_scale(x_train, x_test, n_dim)
+
+	train_vecs, test_vecs = tfidf(train_vecs, test_vecs)
+	print train_vecs[:5]
+
+	lr = SGDClassifier(loss='log', penalty='l1')
+	lr = SGDClassifier()
+	parameters = {'loss':('log', 'hinge', 'squared_hinge', 'modified_huber'),
+				 'alpha':[.3,.6,1,1.3,1.6,2],
+				 'penalty':['none', 'l2', 'l1', 'elasticnet']}
+	clf = GridSearchCV(lr, parameters, cv=5)
+	# clf.fit([extract_features(w) for w in X_train], Y_train)
+	clf.fit(train_vecs, y_train)
+	print "Best accuracy score:"
+	print clf.best_score_, 'with', clf.best_params_
+	print "\nGrid scores on development set:"
+	for params, mean_score, scores in clf.grid_scores_:
+	    print("%0.3f (+/-%0.03f) for %r" % (mean_score, scores.std() * 2, params))
+	lr = SGDClassifier(loss=clf.best_params_['loss'], alpha=clf.best_params_['alpha'], penalty=clf.best_params_['penalty'])	
+
+	lr.fit(train_vecs, y_train)
+	y_pred = lr.predict(test_vecs)
+
+	# eval(y_test,y_pred)
+	# eval(y_test,y_pred,1)
+	# eval(y_test,y_pred,0)
+	# eval(y_test,y_pred,-1)
+	print 'ACC:', accuracy_score(y_test, y_pred)
+	# print 'Test Accuracy: %.2f'%lr.score(mms.transform(test_vecs), y_test)
+	print 'Test Accuracy: %.2f'%lr.score(test_vecs, y_test)
+
+
+	return test_vecs, y_pred
+
 def plotlen(alltw, filltw):
 	n = len(alltw)
 	c = sns.color_palette()
@@ -80,130 +202,68 @@ def plotlen(alltw, filltw):
 
 	plt.show()
 
+def tfidf(training_data, test_data):
+	tfidf_transformer = TfidfTransformer()
+	train_vecs = tfidf_transformer.fit_transform(training_data).todense()
+	test_vecs = tfidf_transformer.transform(test_data).todense()
+	return train_vecs, test_vecs
+
+def eval(real,pred,sent="none"):
+	correct = 0
+	if sent == "none":
+		total = len(real)
+		for i in xrange(len(real)):
+			if  pred[i] == real[i]:
+				correct += 1
+	else:
+		total = 0
+		for i in xrange(len(real)):
+			if real[i] == sent:
+				total += 1
+				if  pred[i] == sent:
+					correct += 1
+
+	print "Correctly predicted " + str(correct/(1.0*total)) + " for sent " + str(sent)
+
+def plotRoc(test_vecs, y_test, pred_probas,n):
+	'''
+	plot ROC curve
+	'''
+
+	c = sns.color_palette()
+
+	fpr,tpr,_ = roc_curve(y_test, pred_probas)
+	roc_auc = auc(fpr,tpr)
+	plt.plot(fpr,tpr,label='area = %.2f' %roc_auc, color=c[n])
+
+		
+	plt.plot([0, 1], [0, 1], 'k--')
+	plt.xlim([0.0, 1.0])
+	plt.ylim([0.0, 1.05])
+	plt.xlabel('False Positive Rate')
+	plt.ylabel('True Positive Rate')
+	plt.legend(loc='lower right')
+
 
 if __name__ == '__main__':
 
-	filename = os.path.join('airline-twitter-sentiment','Tweets.csv')
-	tweets = pd.read_csv(filename)
-
-	tw_text = tweets.text
-	tw_name = tweets.name
-	tw_sent = tweets.airline_sentiment
-	tw_sent_conf = tweets.airline_sentiment_confidence
-	tw_air = tweets.airline
-	tw_rt = tweets.retweet_count
-	tw_coord = tweets.tweet_coord
-	tw_time = tweets.tweet_created
-	tw_loc = tweets.tweet_location
-
-
-	pos_text = tweets.text[tw_sent == "positive"]#+tw_air[tweets.airline_sentiment == "positive"]
-	neu_text = tweets.text[tw_sent == "neutral" ]#+tw_air[tweets.airline_sentiment == "neutral"] 
-	neg_text = tweets.text[tw_sent == "negative"]#+tw_air[tweets.airline_sentiment == "negative"]
-
-	alltw = [len(neg_text),len(pos_text),len(neu_text)]
-	#print "pos:",len(pos_text), "neu:", len(neu_text), "neg:" , len(neg_text)
-
-	pos_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "positive" and tw_sent_conf[i] == 1.0]
-	neu_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "neutral"  and tw_sent_conf[i] == 1.0]
-	neg_text = [t for i,t in enumerate(tweets.text) if tw_sent[i] == "negative" and tw_sent_conf[i] == 1.0]
-
-	filtw = [len(neg_text),len(pos_text),len(neu_text)]
-	#print "pos:", len(pos_text), "neu:", len(neu_text), "neg:" ,len(neg_text)
-
-	plotlen(alltw, filtw)
+	pos_text, neu_text, neg_text = getSentiment_Tw(False)
+	conf_pos_text, conf_neu_text, conf_neg_text = getSentiment_Tw(True)	
 	
-
-	#use 1 for positive sentiment, -1 for negative, 0 for neutral
-	y = np.concatenate((np.ones(len(pos_text)), np.zeros(len(neu_text)), np.full(len(neg_text),-1, dtype=np.int)))
-	#y = np.concatenate((np.ones(len(pos_text)), np.zeros(len(neu_text))))
-	#y = np.concatenate((np.ones(len(pos_text)), np.zeros(len(neg_text))))
-	#y = np.concatenate((np.zeros(len(neg_text)), np.ones(len(neu_text))))
-
-	x_train, x_test, y_train, y_test = train_test_split(np.concatenate((pos_text, neu_text, neg_text)), y, test_size=0.2, random_state=0)
-	#x_train, x_test, y_train, y_test = train_test_split(np.concatenate((pos_text, neu_text)), y, test_size=0.2, random_state=0)
-	#x_train, x_test, y_train, y_test = train_test_split(np.concatenate((pos_text, neg_text)), y, test_size=0.2, random_state=0)
-	#x_train, x_test, y_train, y_test = train_test_split(np.concatenate((neu_text, neg_text)), y, test_size=0.2, random_state=0)
+	#crossvalidation
+	x_train, x_test, y_train, y_test = get_train_test(conf_pos_text, conf_neg_text)
 
 	stopWords = getStopWordList('stopwords.txt')
 
 	x_train = cleanText(x_train, stopWords)
 	x_test = cleanText(x_test, stopWords)
 
-	#print x_train
-
-	n_dim = 400
-	#Initialize model and build vocab
-	#min_count = ignore all words with total frequency lower than this.
-	tw_w2v = Word2Vec(size=n_dim, min_count=50, negative=0, window=5) #size = size of the NN layer
-	tw_w2v.build_vocab(x_train)
-
-	#Train the model 
-	tw_w2v.train(x_train)
+	#initialize w2vec - standardization - classification
+	test_vecs, pred_probas = classify(x_train, y_train, x_test, y_test)
 
 
-	train_vecs = np.concatenate([buildWordVector(z, n_dim) for z in x_train])
-	train_vecs = scale(train_vecs)
-
-	#y = tsne.tsne(train_vecs, 2)
+	if roc == True:
+		plotRoc(test_vecs, y_test, pred_probas,1)
 
 
-	#Train word2vec on test tweets
-	#tw_w2v.train(x_test)
-
-	#Build test tweet vectors then scale
-	test_vecs = np.concatenate([buildWordVector(z, n_dim) for z in x_test])
-	test_vecs = scale(test_vecs)
-
-	
-
-	lr = SGDClassifier(loss='log', penalty='l1')
-	lr.fit(train_vecs, y_train)
-
-
-	print 'Test Accuracy: %.2f'%lr.score(test_vecs, y_test)
-
-	# for i in xrange(20):
-	# 	print lr.predict(test_vecs[i]), x_test[i]
-	# # print "\n",tw_w2v.most_similar('wonderful')
-	# # print "\n",tw_w2v.most_similar('bad')
-
-
-
-	#############################plot ROC #########################################
-
-	# n_classes = 3
-	# pred_probas = lr.predict_proba(test_vecs)[:,1]
-	# print pred_probas.shape
-	# print y_test.shape
-
-	# plt.figure()
-
-	# # fpr = dict()
-	# # tpr = dict()
-	# # roc_auc = dict()
-
-	# # for i in range(n_classes):
-	# # 	fpr[i], tpr[i], _ = roc_curve(y_test, y_score[:, i])
-	# # 	roc_auc[i] = auc(fpr[i], tpr[i])
-
-	# # for i in range(n_classes):
-	# # 	plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.2f})'
-	# # 								   ''.format(i, roc_auc[i]))
-
-	# fpr,tpr,_ = roc_curve(y_test, pred_probas)
-	# roc_auc = auc(fpr,tpr)
-	# plt.plot(fpr,tpr,label='area = %.2f' %roc_auc)
-
-		
-	# plt.plot([0, 1], [0, 1], 'k--')
-	# plt.xlim([0.0, 1.0])
-	# plt.ylim([0.0, 1.05])
-	# plt.xlabel('False Positive Rate')
-	# plt.ylabel('True Positive Rate')
-	# plt.legend(loc='lower right')
-		
-	# plt.show()
-
-	#########################################
-
+	plt.show()
